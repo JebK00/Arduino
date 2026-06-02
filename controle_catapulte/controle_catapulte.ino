5f50f4d5704029ab1c836799dc1da0809506bfe9
Vemcros <86977267+JebK00@users.noreply.github.com>
2026-06-02 08:54:18 +0200

    Micron

--- controle_catapulte/controle_catapulte.ino
+++ controle_catapulte/controle_catapulte.ino
@@ -1,32 +1,56 @@
-const int switchPinB = 1;
-const int switchPinRB = 2;
-const int enablePin = 7;
-const int directionSwitchPin = 7;
-int motorState = LOW;
-int buttonStateB;
-int buttonStateRB;
-int lastButtonStateB = LOW;
-int lastButtonStateRB = LOW;
-int allowRB = 0;
+const int controlPin1 = 2;
+const int controlPin2 = 3;
+const int enablePin = 9;
+const int directionSwitchPin = 4;
+const int onOffSwitchStateSwitchPin = 5;
+
+int onOffSwitchState = 0;
+int previousOnOffSwitchState = 0;
+int directionSwitchState = 0;
+int previousDirectionSwitchState = 0;
+
+int motorEnabled = 0;
+int motorSpeed = 0;
+int motorDirection = 1;
 
 void setup(){
-  pinMode(motorPin, OUTPUT);
-  pinMode(switchPinB, INPUT);
-  pinMode(switchPinRB, INPUT);
+  pinMode(directionSwitchPin, INPUT);
+  pinMode(onOffSwitchStateSwitchPin, INPUT);
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW);
 }
 
-void loop() {
-  buttonStateB = digitalRead(switchPin);
-  buttonStateRB = digitalRead(switchPin);
-  if (buttonStateB == HIGH && lastButtonState == LOW){
-    if (motorState == HIGH){
-      motorState = LOW;
+void loop(){
+  onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+  delay(1);
+  directionSwitchState = digitalRead(directionSwitchPin);
+  motorSpeed = HIGH;
+  if(onOffSwitchState != previousOnOffSwitchState){
+    if(onOffSwitchState == HIGH){
+      motorEnabled = !motorEnabled;
     }
-    else {
-      motorState = HIGH;
-    }
-    digitalWrite(motorPin, motorState);
-    delay(50);
   }
-  lastButtonState = buttonState;
+  if (directionSwitchState != previousDirectionSwitchState) {
+    if (directionSwitchState == HIGH) {
+      motorDirection = !motorDirection;
+    }
+  }
+  if (motorDirection == 1) {
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
+  }
+  else {
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, HIGH);
+  }
+  if (motorEnabled == 1) {
+    analogWrite(enablePin, motorSpeed);
+  }
+  else {
+    analogWrite(enablePin, 0);
+  }
+  previousDirectionSwitchState = directionSwitchState;
+  previousOnOffSwitchState = onOffSwitchState;
 }
