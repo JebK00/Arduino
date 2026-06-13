c741dc276b5a9394341e4ac2a20782cc0869a5dc
Vemcros <86977267+JebK00@users.noreply.github.com>
2026-06-13 23:11:41 +0200

    Jacobien

--- combi/combi.ino
+++ combi/combi.ino
@@ -1,18 +1,14 @@
-/// initiate constant variable for control of right motor
 const int MD_controlPin1 = 2;
 const int MD_controlPin2 = 3;
 const int MD_enablePin = 4;
 
-/// initiate constant variable for control of left motor
 const int MG_controlPin1 = 8;
 const int MG_controlPin2 = 9;
 const int MG_enablePin = 10;
 
-/// initiate motors status; 0 = off   1 = on 
 int MD_status = 0;
 int MG_status = 0;
 
-/// initiate motors direction; 0 = forward   1 = rearward
 int MD_direction = 0;
 int MG_direction = 0;
 
@@ -37,6 +33,15 @@ int motorEnabled = 0;
 int motorSpeed = 0;
 int motorDirection = 1;
 
+int buttonState = 0;
+int previousButtonState = 0;
+int robotEnabled = 0;
+
+int embobState = 0;
+bool temp = LOW;
+
+int a = 0;
+
 void setup() {
   pinMode(MD_controlPin1, OUTPUT);
   pinMode(MD_controlPin2, OUTPUT);
@@ -53,41 +58,71 @@ void setup() {
   pinMode(enablePin, OUTPUT);
   digitalWrite(enablePin, LOW);
 
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  lumG = analogRead(lumGPin);
-  lumD = analogRead(lumDPin);
-  Serial.print("lumG: ");
-  Serial.print(lumG);
-  Serial.print(" - lumD: ");
-  Serial.println(lumD);
-  digitalWrite(MD_controlPin1, HIGH);
-  digitalWrite(MD_controlPin2, LOW);
-  digitalWrite(MG_controlPin1, HIGH);
-  digitalWrite(MG_controlPin2, LOW);
-  analogWrite(MD_enablePin, 150);
-  analogWrite(MG_enablePin, 150);
-  if (lumG<100 & (min(lumG, lumD) == lumG)){
-    Serial.println("Gauche");
+  embobState = digitalRead(embobPin);
+  if(embobState == HIGH){
+    temp = HIGH;
+  }
+  buttonState = digitalRead(buttonPin);
+  delay(1);
+  if (buttonState != previousButtonState) {
+    if (buttonState == HIGH) {
+      robotEnabled = !robotEnabled;
+      if (robotEnabled == 1) {
+        a = 0; 
+      }
+    }
+  }
+  if (robotEnabled == 1) {
+    lumG = analogRead(lumGPin);
+    lumD = analogRead(lumDPin);
+    Serial.print("lumG: ");
+    Serial.print(lumG);
+    Serial.print(" - lumD: ");
+    Serial.println(lumD);
     digitalWrite(MD_controlPin1, HIGH);
     digitalWrite(MD_controlPin2, LOW);
-    digitalWrite(MG_controlPin1, LOW);
-    digitalWrite(MG_controlPin2, HIGH);
-    analogWrite(MD_enablePin, 120);
-    analogWrite(MG_enablePin, 120);
-  }
-  if (lumD<100 & (min(lumG, lumD) == lumD)){
-    Serial.println("Droite");
-    digitalWrite(MD_controlPin1, LOW);
-    digitalWrite(MD_controlPin2, HIGH);
     digitalWrite(MG_controlPin1, HIGH);
     digitalWrite(MG_controlPin2, LOW);
-    analogWrite(MD_enablePin, 120);
-    analogWrite(MG_enablePin, 120);
+    analogWrite(MD_enablePin, 150);
+    analogWrite(MG_enablePin, 150);
+    if (lumG<100 & (min(lumG, lumD) == lumG)){
+      Serial.println("Gauche");
+      digitalWrite(MD_controlPin1, HIGH);
+      digitalWrite(MD_controlPin2, LOW);
+      digitalWrite(MG_controlPin1, LOW);
+      digitalWrite(MG_controlPin2, HIGH);
+      analogWrite(MD_enablePin, 120);
+      analogWrite(MG_enablePin, 120);
+    }
+    if (lumD<100 & (min(lumG, lumD) == lumD)){
+      Serial.println("Droite");
+      digitalWrite(MD_controlPin1, LOW);
+      digitalWrite(MD_controlPin2, HIGH);
+      digitalWrite(MG_controlPin1, HIGH);
+      digitalWrite(MG_controlPin2, LOW);
+      analogWrite(MD_enablePin, 120);
+      analogWrite(MG_enablePin, 120);
+    }
+    delay(500);
   }
-  delay(500); 
+  else {
+    analogWrite(MD_enablePin, 0);
+    analogWrite(MG_enablePin, 0);
+    digitalWrite(MD_controlPin1, LOW);
+    digitalWrite(MD_controlPin2, LOW);
+    digitalWrite(MG_controlPin1, LOW);
+    digitalWrite(MG_controlPin2, LOW);
+  }
+  if (temp = HIGH){
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
+    analogWrite(enablePin, HIGH);
+    delay(5000);
+    temp = LOW;
+  }
+  previousButtonState = buttonState;
 }
